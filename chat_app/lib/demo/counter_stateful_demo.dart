60887a3572e8c742940b5c08cc5c1226493f5b96
Dheeraj Singh <sanctificarespartacus@gmail.com>
2024-01-08 14:31:17 +0530

    Separating out login page and adding basic widgets

--- chat_app/lib/demo/counter_stateful_demo.dart
+++ chat_app/lib/demo/counter_stateful_demo.dart
@@ -1,2 +1,31 @@
-
+import 'package:flutter/material.dart';
 //TODO: Recreate the counter app
+
+class CounterStateful extends StatefulWidget {
+  const CounterStateful({Key? key}) : super(key: key);
+
+  @override
+  State<CounterStateful> createState() {
+    State<CounterStateful> stateClassAssociatedWithThisWidget =
+        _CounterStatefulState();
+    return stateClassAssociatedWithThisWidget;
+  }
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment(){
+    setState(() {
+      counter++;
+    });
+    print(counter);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton:
+            FloatingActionButton(child: Icon(Icons.add), onPressed: () {increment();}),
+        body: Center(child: Text('$counter', style: TextStyle(fontSize: 30))));
+  }
+}
