3fc467528853f6d9b883b6652f7a15434e67f576
Dheeraj Singh <sanctificarespartacus@gmail.com>
2024-01-09 16:04:07 +0530

    Separating out login page and adding basic widgets

--- chat_app/lib/utils/textfield_styles.dart
+++ chat_app/lib/utils/textfield_styles.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class ThemeTextStyle{
-  static TextStyle loginTextFieldStyle = TextStyle(color: Colors.blueGrey);
+class ThemeTextStyle {
+  static TextStyle loginTextFieldStyle =
+      GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey));
 }
